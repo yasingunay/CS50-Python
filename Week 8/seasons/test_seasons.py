ab2fd009741e71ea973bdd4d6f7e5f55dc0ed7df
Yasin Günay <88950012+yasingunay@users.noreply.github.com>
2023-08-04 13:45:54 +0200

    Update test_seasons.py

--- Week 8/seasons/test_seasons.py
+++ Week 8/seasons/test_seasons.py
@@ -4,9 +4,9 @@ def main():
     test_check_birth()
 
 def test_check_birth():
-    assert check_birth('1994-07-04') == ("1994", "07", "04")
-    assert check_birth('1994-7-4') == None
-    assert check_birth("July 4, 1994") == None
+    assert check_birth('1990-01-01') == ("1990", "01", "01")
+    assert check_birth('1990-1-1') == None
+    assert check_birth("July 1, 1991") == None
 
 
 if __name__ =="__main__":
